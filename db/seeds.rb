ef8ac4aaccff5b381227bd05a74ac2f5425e3f59
github username <schun08@gmail.com>
2018-06-12 22:11:47 -0400

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Location.create(city: "Morrison", state: "Colorado")
 Location.create(city: "Brooklyn", state: "New York")
 Location.create(city: "New York", state: "New York")
@@ -12,7 +5,11 @@ Location.create(city: "New York", state: "New York")
 Venue.create(name: "Red Rocks Amphitheater", location_id: 1, avg_rating: 5, capacity: 9525)
 Venue.create(name: "Brooklyn Steel", location_id: 2, avg_rating: 3.5, capacity: 1800)
 Venue.create(name: "Madison Square Garden", location_id: 3, avg_rating: 4, capacity: 20789)
+Venue.create(name: "Irving Plaza", location_id: 3, avg_rating: 4, capacity: 1025)
 
-Suggestion.create(venue_id: 2, location_id: 2, name: "One Stop Beer Shop", avg_price: 15, upvotes: 25, downvotes: 2)
+Suggestion.create(venue_id: 2, location_id: 2, name: "One Stop Beer Shop", category: "Bar", avg_price: 15, upvotes: 25, downvotes: 2)
+Suggestion.create(venue_id: 4, location_id: 3, name: "Artichoke Pizza", category: "Quick Bite", avg_price: 10, upvotes: 100, downvotes: 15)
+Suggestion.create(venue_id: 4, location_id: 3, name: "The Halal Guys", category: "Quick Bite", avg_price: 10, upvotes: 1000, downvotes: 15)
+Suggestion.create(venue_id: 3, location_id: 3, name: "McGarry's Bar and Restaurant", category: "Bar", avg_price: 10, upvotes: 35, downvotes: 40)
 
 Review.create(venue_id: 1, content: "Gorgeous venue", rating: 5)
