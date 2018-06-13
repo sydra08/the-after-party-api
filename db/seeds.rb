63fc4bd34228db085418c64f8899bb222702ffc3
github username <schun08@gmail.com>
2018-06-12 21:59:14 -0400

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Location.create(city: "Morrison", state: "Colorado")
+Location.create(city: "Brooklyn", state: "New York")
+Location.create(city: "New York", state: "New York")
+
+Venue.create(name: "Red Rocks Amphitheater", location_id: 1, avg_rating: 5, capacity: 9525)
+Venue.create(name: "Brooklyn Steel", location_id: 2, avg_rating: 3.5, capacity: 1800)
+Venue.create(name: "Madison Square Garden", location_id: 3, avg_rating: 4, capacity: 20789)
+
+Suggestion.create(venue_id: 2, location_id: 2, name: "One Stop Beer Shop", avg_price: 15, upvotes: 25, downvotes: 2)
+
+Review.create(venue_id: 1, content: "Gorgeous venue", rating: 5)
