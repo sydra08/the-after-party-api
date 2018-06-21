2a209879492e770946f10cf9ab38c191732ec733
github username <schun08@gmail.com>
2018-06-21 08:53:59 -0400

    add create and update methods to Suggestions Controller

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,14 +5,28 @@ class SuggestionsController < ApplicationController
   end
 
   def create
+    @venue = Venue.find_by(id: params[:venue_id])
+    @suggestion = @venue.suggestions.build(suggestion_params)
+    if @suggestion.save
+      # 201 means created
+      render json: @suggestion, status: 201
+    else
+      # 400 means bad request
+      render json: @suggestion, status: 400
   end
 
   def update
+    @suggestion = Suggestion.find_by(id: params[:id])
+    if @suggestion.update(suggestion_params)
+      render json: @suggestion, status: 200
+    else
+      render json: @suggestion, status: 404
   end
 
   private
 
   def suggestion_params
+    params.require(:suggestion).permit(:name, :category, :avg_price, :upvotes, :downvotes)
   end
 
 end
