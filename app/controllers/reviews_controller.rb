2f3961c9eb2e548a74a2e2c672be7acd092c57a3
github username <schun08@gmail.com>
2018-06-21 08:36:00 -0400

    add create method to Reviews Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,14 +5,25 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @venue = Venue.find_by(id: params[:venue_id])
+    @review = @venue.reviews.build(review_params)
+    if @review.save
+      # 201 means created
+      render json: @review, status: 201
+    else
+      # 400 means bad request
+      render json: @review, status: 400
   end
 
   def update
+    # should return 200 or 400
+    # 200 means OK
   end
 
   private
 
   def review_params
+    params.require(:review).permit(:content, :rating)
   end
 
 end
